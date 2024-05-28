bec34e774a19538162c4a7293a913abdc920599f
bitraedev <dev@bitrae.net>
2024-05-28 22:24:46 +0100

    bump package version

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -18,10 +18,10 @@
 #define CLIENT_VERSION_MAJOR 0
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 21
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 3
+#define CLIENT_VERSION_REVISION 0
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
@@ -253,7 +253,7 @@
 #define PACKAGE_NAME "Bitrae Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Bitrae Core 0.21.3"
+#define PACKAGE_STRING "Bitrae Core 0.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bitrae"
@@ -262,7 +262,7 @@
 #define PACKAGE_URL "https://bitrae.net/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.21.3"
+#define PACKAGE_VERSION "0.1.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
